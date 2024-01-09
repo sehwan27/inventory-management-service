755a2773181d7d0592360e2db157e16f07590bd2
Seh Wan Loong <sehwan1997@hotmail.com>
2024-01-09 14:10:11 +0800

    authentication in product routes

---
+++ middleware/authentication.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const authentication = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(401).send("Missing token");
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, "secretKey", (err: any, decoded: any) => {
+      if (err) return res.status(401).send("Unauthorized");
+      next();
+    });
+  } catch (error) {
+    res.status(401).send("Unauthorized");
+  }
+};
--- routes/index.ts
+++ routes/index.ts
@@ -4,9 +4,10 @@ const router = Router();
 import categoryRoutes from './categoryRoutes'
 import productRoutes from './productRoutes'
 import userRoutes from './userRoutes'
+import { authentication } from "../middleware/authentication";
 
 router.use('/', categoryRoutes)
-router.use('/', productRoutes)
+router.use('/', authentication, productRoutes)
 router.use('/', userRoutes)
 
 export default router;
